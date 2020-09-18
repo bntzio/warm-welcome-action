49efd64632723bdcfc87e20d00096b55f0517f6d
Enrique Benitez <enriquebenitezjr@gmail.com>
2020-09-17 23:32:21 -0500

    Add action functionality

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,52 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
+import axios from 'axios';
 
-function run() {
-  const name: string = core.getInput('my_input');
-  if (name) {
-    core.debug(`Hello ${name}!`);
-    return core.setOutput('my_output', `Hello ${name}!`);
+async function fetchGif(): Promise<string> {
+  try {
+    let gif = ''
+
+    const giphyToken = core.getInput('GIPHY_TOKEN')
+
+    if (giphyToken) {
+      const response = await axios.get(`https://api.giphy.com/v1/gifs/random?api_key=${giphyToken}&tag=welcome`)
+      gif = `![welcome_gif](${response.data.data.image_url})`;
+    }
+
+    return gif;
+  } catch(e) {
+    core.setFailed(e.message);
+    return '';
   }
-  core.setFailed('my_input not specified!');
+}
+
+async function run(): Promise<void> {
+  try {
+    const githubToken = core.getInput('GITHUB_TOKEN');
+
+    const context = github.context;
+
+    if (context.payload.pull_request == null && context.payload.issue == null) {
+      core.setFailed('No pull request found.');
+      return;
+    }
+
+    const gif = await fetchGif();
+
+    const requestNumber = context.payload.pull_request?.number || context.payload.issue?.number;
+
+    const octokit = github.getOctokit(githubToken);
+
+    const response = await octokit.issues.createComment({
+      ...context.repo,
+      issue_number: requestNumber!,
+      body: gif
+    });
+
+    core.setOutput('gif_url', response.data.html_url);
+  } catch(e) {
+    core.setFailed(e.message);
+  }
 }
 
 run();
